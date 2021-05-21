96decfaf03692a156a1372fcf85784b26346483a
bashasalam <mitbashaeee@gmail.com>
2021-05-20 19:27:23 -0700

    Sprotifyshoes project created.
    This is the initial commit

---
+++ sportifyshoes/sportifyshoes/src/main/java/com/springmvc/sportifyshoes/SportifyshoesApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.sportifyshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportifyshoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportifyshoesApplication.class, args);
+	}
+
+}
---
+++ sportifyshoes/sportifyshoes/src/test/java/com/springmvc/sportifyshoes/SportifyshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.sportifyshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportifyshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
